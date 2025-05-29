c632a4d2df306922838463c495769063c33fe94a
raylann-lopes <raylanlopes@icloud.com>
2025-05-29 20:03:46 -0300

    feat: add login config

--- src/app/authentication/components/login-form.tsx
+++ src/app/authentication/components/login-form.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -22,6 +25,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const loginSchema = z.object({
   email: z
@@ -35,6 +39,7 @@ const loginSchema = z.object({
 });
 
 const LoginForm = () => {
+  const Router = useRouter();
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -43,8 +48,25 @@ const LoginForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof loginSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+      },
+      {
+        onSuccess: () => {
+          // Redirecionar ou realizar alguma ação após o login bem-sucedido
+          Router.push("/dashboard"); // Exemplo de redirecionamento
+        },
+        onError: () => {
+          toast.error("Email ou senha inválidos", {
+            description:
+              "Por favor, verifique suas credenciais e tente novamente.",
+          });
+        },
+      },
+    );
   }
 
   return (
@@ -95,7 +117,17 @@ const LoginForm = () => {
             />
           </CardContent>
           <CardFooter>
-            <Button type="submit">Submit</Button>
+            <Button
+              type="submit"
+              className="w-full"
+              disabled={form.formState.isSubmitting}
+            >
+              {form.formState.isSubmitting ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                "Entrar"
+              )}
+            </Button>
           </CardFooter>
         </form>
       </Form>
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+const DashboardPage = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-4 text-4xl font-bold">Dashboard</h1>
+      <p className="text-lg">Welcome to your dashboard!</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
