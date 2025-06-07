e8cc60539baef581bdf84e2b547108809b143be3
raylann-lopes <raylanlopes@icloud.com>
2025-06-06 21:25:02 -0300

    feat: add patient table

---
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+type Patient = typeof patientsTable.$inferSelect;
+
+export const PatientTableColumns: ColumnDef<Patient>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    id: "phoneNumber",
+    accessorKey: "phoneNumber",
+    header: "Telefone",
+    cell: ({ row }) => {
+      const patient = row.original;
+      const phone = patient.phoneNumber;
+
+      if (!phone) return "-";
+
+      // Format: (XX) XXXXX-XXXX
+      const formatted = phone.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3");
+
+      return formatted;
+    },
+  },
+  {
+    id: "sexo",
+    accessorKey: "sexo",
+    header: "Sexo",
+
+    cell: ({ row }) => {
+      const patient = row.original;
+      return patient.sexo === "male" ? "Masculino" : "Feminino";
+    },
+  },
+  {
+    id: "actions",
+    header: "Ações",
+    cell: (params) => {
+      const patient = params.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" size="icon" className="cursor-pointer">
+              <MoreVerticalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              <span className="font-bold">{patient.name}</span>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer">
+              <EditIcon /> Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <TrashIcon /> Excluir
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
